4f4fda37519a1f9e76fe714295ed7c9e82d74c54
tanapi-e <ryota.wavershrinp@icloud.com>
2020-07-12 10:46:06 +0900

    試合予定削除処理実装

--- resources/views/cms/game/listGameScheduleInfo.blade.php
+++ resources/views/cms/game/listGameScheduleInfo.blade.php
@@ -15,9 +15,29 @@
                 <tr>
                     <td>{{ $game->opponent_team }}</td>
                     <td>{{ $game->match_day }}</td>
+                    <td>
+                        <form action="/cms/game/schedule/delete/{{ $game->id }}" method="post">
+                            {{ csrf_field() }}
+                            <input type="submit" value="削除" class="btn btn-danger btn-sm btn-dell">
+                        </form>
+                    </td>
                 </tr>
             @endforeach
         </tbody>
     </table>
 </div>
+@endsection
+
+@section('script')
+    <script>
+        $(function() {
+            $('.btn-dell').click(function() {
+                if (confirm('本当に削除しますか？')) {
+                    alert('削除しました。');
+                } else {
+                    return false;
+                }
+            });
+        });
+    </script>
 @endsection
