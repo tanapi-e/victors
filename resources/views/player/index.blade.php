420a55607c1f7f6e443bf3664a610aeb9f29de87
tanapi-e <ryota.wavershrinp@icloud.com>
2020-10-06 00:56:31 +0900

    選手情報ページ実装

--- resources/views/player/index.blade.php
+++ resources/views/player/index.blade.php
@@ -3,11 +3,26 @@
 @section('content')
 <div id="wrapper">
     <h2 class="title">所属選手</h2>
-    <ul>
-        <!-- 仮作成 -->
-        @foreach ($playerList as $player)
-            <li>{{ $player->uniform_number }}{{ $player->name }}</li>
-        @endforeach
-    </ul>
+    <div class="playerPageArea">
+        <ul class="playerUl">
+            <!-- 仮作成 -->
+            @foreach ($playerList as $player)
+            <li class="playerList">
+                <div class="playerInfo">
+                    <span class="uniNumner">
+                        {{ $player->uniform_number }}
+                    </span>
+                    <p class="profile">
+                        <strong>{{ $player->name }}</strong>
+
+                        <span>
+                            {{ $player->position }} / {{ $player->age }} / {{ $player->management_position }}
+                        </span>
+                    </p>
+                </div>
+            </li>
+            @endforeach
+        </ul>
+    </div>
 </div>
 @endsection
